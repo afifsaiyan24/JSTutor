c1585ae24213457d23c12c8d53463a3065e02551
afifsaiyan24 <afifsaiyan24@gmail.com>
2025-12-03 07:02:29 +0700

    Javascript_Tutor5

---
+++ main4.js
@@ -0,0 +1,17 @@
+for (let a = 1; a <= 5; a++){
+    console.log('data ke-',a)
+}
+
+let b = 1
+while (b <= 5){
+    console.log('data ke-',b)
+    b++
+}
+
+let c = 1
+do{
+    console.log('data ke-',c)
+    c++
+} while (c <= 10)
+
+
